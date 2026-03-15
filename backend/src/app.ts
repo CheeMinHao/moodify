58b31691d7250a0d95dc4863c4133b8c13ca34b2
CheeMinHao <cheemh3456@gmail.com>
2026-03-15 11:44:16 +1100

    aws s3 audio signed link bugfix and re-seeded elastic db with new data

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,24 @@
 const express = require('express');
 const apiRoutes = require('./routes/api.routes');
 import { env } from './config/env';
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 const app = express();
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My Express.js API',
+      version: '1.0.0',
+      description: 'A sample Express.js API built with TypeScript and Swagger',
+    },
+  },
+  apis: ['./src/routes/*.ts'],  
+};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 
